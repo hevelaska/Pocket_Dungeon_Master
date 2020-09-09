1c7955140144bfff54b39bd084d01e98d9860f59
hevelaska <55829442+hevelaska@users.noreply.github.com>
2020-09-09 18:44:14 +0200

    config bouton annuler de GenerateEncounterFormActivity

--- app/src/main/java/be/hevelaska/pdm/GenerateEncounterFormActivity.java
+++ app/src/main/java/be/hevelaska/pdm/GenerateEncounterFormActivity.java
@@ -1,14 +1,10 @@
 package be.hevelaska.pdm;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 
 import be.hevelaska.pdm.databinding.ActivityGenerateEncounterFormBinding;
-import be.hevelaska.pdm.databinding.ActivityMonsterBinding;
 
 public class GenerateEncounterFormActivity extends AppCompatActivity {
 
@@ -20,5 +16,8 @@ public class GenerateEncounterFormActivity extends AppCompatActivity {
         binding = ActivityGenerateEncounterFormBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.btnCancel.setOnClickListener(view -> finish());
     }
+
+
 }
